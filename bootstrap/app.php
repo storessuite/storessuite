8effcaf73963f2fbf26455a3fd01d7eff46f93b2
Saroj Kala Magar <sarojkalamagar@gmail.com>
2024-12-28 13:29:49 +0545

    Auth scaffolding - breeze + inertia + reactjs

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,11 @@ return Application::configure(basePath: dirname(__DIR__))
         }
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->web(append: [
+            \App\Http\Middleware\HandleInertiaRequests::class,
+            \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+        ]);
+
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
